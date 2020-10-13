0eddaa41db65b57e37165aaaa9a1b418eef76e37
Om <36092598+Khetwal-om@users.noreply.github.com>
2020-10-13 11:52:07 +0530

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { Button } from './Button'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+import Dropdown from './Dropdown'
+
+function Navbar() {
+  const [click, setClick] = useState(false)
+  const [dropdown, setDropdown] = useState(false)
+  const closeMobileMenu = () => setClick(false)
+  const handleClick = () => setClick(!click)
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false)
+    } else {
+      setDropdown(true)
+    }
+  }
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false)
+    } else {
+      setDropdown(false)
+    }
+  }
+
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo">
+          EPIC
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li
+            className="nav-item"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link
+              to="/services"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Services <i className="fas fa-caret-down"></i>
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact-us"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              ContactUS
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
